99c8eb274c35350f309de6a40b7d7f0a9db57f0c
mark chaitin <mchaitin@gmail.com>
2020-10-06 07:45:56 -0500

    adjusted clock stuff

--- src/main.rs
+++ src/main.rs
@@ -17,20 +17,15 @@ fn main() {
   if &pattern == "parser" {
     debug!("Initialized in parser mode");
     debug!("Disabled for the moment");
-  // let mut parser = parser::Parser::new();
-  // parser.run();
   } else {
     let (tx, rx) = mpsc::channel();
-    let clock = thread::spawn(move || loop {
+    thread::spawn(move || loop {
       thread::sleep(Duration::from_micros(1790));
       tx.send(true).unwrap();
     });
     debug!("Initialized in program mode");
     let program = vec![0xA9, 0x10, 0x69, 0x10];
     let mut cpu = CPU::new(rx);
-    let running = cpu.run(program, None);
-    if !running {
-      clock.join().unwrap();
-    }
+    cpu.run(program, None);
   }
 }
